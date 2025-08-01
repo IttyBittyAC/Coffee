df2c2c5ebd4d75fc08514607d59c86358dbf1ebb
elijah <30031044@tafe.wa.edu.au>
2025-08-01 10:31:17 +0800

    done activity

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val coffeeOptions = listOf("coffee", "milk", "vanilla", "sugar")
+    val customerPicks = listOf("coffee", "milk")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println(calculateCaffeineLevel(8, 0, "hopes lost"))
+    if(CanFullFillOrder(coffeeOptions, customerPicks)){
+        println("enter name of drink")
+
     }
+    else{
+        "Order could not be full filled"
+    }
+
+}
+fun CanFullFillOrder(availability: List<String>, customerOrder: List<String>): Boolean {
+    var choices: Int = 0
+    for (picks in customerOrder) {
+         if (availability.contains(picks)) {
+             choices++
+         }
+     }
+    if (choices == customerOrder.size){
+        return true
+    }
+
+    return false
+}
+fun calculateCaffeineLevel(hoursWorked: Int, bugsFound: Int, energy: String): String{
+    when(energy){
+        "tired" -> if (hoursWorked < 8 && bugsFound > 0){return "strong"}
+                    else if(hoursWorked > 8 && bugsFound > 0) {"Jet Fuel"}
+        "awake" -> if (hoursWorked < 6 && bugsFound > 0){return "regular"}
+        "juiced" -> return "weak"
+        "hopes lost" -> return "there is no coffee strong enough"
+        else -> return "unknown"
+    }
+
+    return "Caffeine"
+}
+fun createDrinkName(name: String): String{
+    return "$name has been set as drinks name"
 }
